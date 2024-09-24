d5193110617ebd88d2e911a864826e9749bb5594
DaveOuma <davecarlosotieno@gmail.com>
2024-09-24 23:44:38 +0300

    Fixed permission issues with dump.rdb and updated .gitignore to exclude cache files

--- uem/uem/settings/base.py
+++ uem/uem/settings/base.py
@@ -76,17 +76,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'uem.wsgi.application'
 
-# Database configuration (uncomment and configure for your database)
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('DB_NAME'),
-#         'USER': os.environ.get('DB_USER'),
-#         'PASSWORD': os.environ.get('DB_PASSWORD'),
-#         'HOST': os.environ.get('DB_HOST'),
-#         'PORT': os.environ.get('DB_PORT', '5432'),
-#     }
-# }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
@@ -112,7 +108,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # For production
+STATIC_ROOT = BASE_DIR / 'static'  # For production
 
 # Media files
 MEDIA_URL = 'media/'
--- uem/uem/settings/prod.py
+++ uem/uem/settings/prod.py
@@ -8,15 +8,20 @@ ADMINS = [
     ('David O', 'davidomuga@gmail.com'),
 ]
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['uemproject','www.uemproject.com']
 
 DATABASES = {
    'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': os.environ.get('POSTGRES_DB'),
-       'USER': os.environ.get('POSTGRES_USER'),
-       'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-       'HOST': 'db',
-       'PORT': 5432,
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+       
    }
 }
+
+REDIS_URL = 'redis://cache:6379'
+CACHES['default']['LOCATION'] = REDIS_URL
+CHANNEL_LAYERS['default']['CONFIG']['hosts'] = [REDIS_URL]
