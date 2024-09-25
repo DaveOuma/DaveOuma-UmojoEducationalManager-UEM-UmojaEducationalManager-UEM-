3bef39be9fd09c3f200711e1e50d40f5531b7679
DaveOuma <davecarlosotieno@gmail.com>
2024-09-25 23:21:30 +0300

    Fix Docker Compose Configuration
    - Corrected indentation for the daphne service in docker-compose.yml.
    - Added version specification for Docker Compose.
    - Ensured proper formatting of environment variables for the daphne service.

--- uem/courses/apps.py
+++ uem/courses/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
-
+import os
 
 class CoursesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'courses'
+    path = os.path.join(os.path.dirname(__file__), 'courses')
